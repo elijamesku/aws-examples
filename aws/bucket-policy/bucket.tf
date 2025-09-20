209b31d411f13199caee95dc2cc2c7e80814da67
Eli James <116235057+elijamesku@users.noreply.github.com>
2025-09-19 23:30:14 -0500

    Update bucket.tf

--- aws/bucket-policy/bucket.tf
+++ aws/bucket-policy/bucket.tf
@@ -38,3 +38,7 @@ resource "aws_s3_bucket_policy" "example" {
   bucket = aws_s3_bucket.bucket.id
   policy = data.aws_iam_policy_document.bucket_policy.json
 }
+
+curl -LO https://github.com/cli/cli/releases/download/v2.45.0/gh_2.45.0_macOS_arm64.tar.gz
+tar -xzf gh_2.45.0_macOS_arm64.tar.gz
+mv gh_2.45.0_macOS_arm64/bin/gh ~/bin/
