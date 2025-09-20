40c88404d39ef0fb18ea43e3e6292760130bd8ec
Eli James <116235057+elijamesku@users.noreply.github.com>
2025-09-20 00:10:48 -0500

    Update bucket.tf

--- aws/bucket-policy/bucket.tf
+++ aws/bucket-policy/bucket.tf
@@ -39,32 +39,3 @@ resource "aws_s3_bucket_policy" "example" {
   policy = data.aws_iam_policy_document.bucket_policy.json
 }
 
- 1. Set your Homebrew directory
-export HOMEBREW_PREFIX="$HOME/homebrew"
-
-
-(You can also add this to your .zshrc later.)
-
- 2. Clone Homebrew into your home folder
-git clone https://github.com/Homebrew/brew "$HOMEBREW_PREFIX"
-
-
-This clones the actual Homebrew core scripts — same as the installer would do.
-
- 3. Add Homebrew to your PATH
-
-Add this to your ~/.zshrc:
-
-echo 'export PATH="$HOME/homebrew/bin:$PATH"' >> ~/.zshrc
-echo 'export HOMEBREW_NO_ANALYTICS=1' >> ~/.zshrc
-source ~/.zshrc
-
- 4. Verify it's working
-brew --version
-
-
-You should see something like:
-
-Homebrew 4.x.x
-
-5. Try installing a package
