455e45af3d1ef4d306d7b782b8ea9a7a59368346
Eli James <116235057+elijamesku@users.noreply.github.com>
2025-09-19 23:39:14 -0500

    Update bucket.tf

--- aws/bucket-policy/bucket.tf
+++ aws/bucket-policy/bucket.tf
@@ -39,6 +39,32 @@ resource "aws_s3_bucket_policy" "example" {
   policy = data.aws_iam_policy_document.bucket_policy.json
 }
 
-curl -LO https://github.com/cli/cli/releases/download/v2.45.0/gh_2.45.0_macOS_arm64.tar.gz
-tar -xzf gh_2.45.0_macOS_arm64.tar.gz
-mv gh_2.45.0_macOS_arm64/bin/gh ~/bin/
+ 1. Set your Homebrew directory
+export HOMEBREW_PREFIX="$HOME/homebrew"
+
+
+(You can also add this to your .zshrc later.)
+
+ 2. Clone Homebrew into your home folder
+git clone https://github.com/Homebrew/brew "$HOMEBREW_PREFIX"
+
+
+This clones the actual Homebrew core scripts — same as the installer would do.
+
+ 3. Add Homebrew to your PATH
+
+Add this to your ~/.zshrc:
+
+echo 'export PATH="$HOME/homebrew/bin:$PATH"' >> ~/.zshrc
+echo 'export HOMEBREW_NO_ANALYTICS=1' >> ~/.zshrc
+source ~/.zshrc
+
+ 4. Verify it's working
+brew --version
+
+
+You should see something like:
+
+Homebrew 4.x.x
+
+5. Try installing a package
